227fa3510bd8d11e155d3fe7800b70015fa54bd5
Simon_Chen <1020359403@qq.com>
2020-07-07 16:10:50 +0800

    change setup and try upload to testpypi

--- setup.py
+++ setup.py
@@ -8,8 +8,19 @@ setup(
     name="ojpacker",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=['rich>=3'],
+    entry_points={
+        'console_scripts': ['ojpacker = ojpacker:main'],
+    },
+    # requires
     python_requires='>=3.6.9',
+    install_requires=[
+        'rich>=3',
+    ],
+    # description
+    description=
+    "a script can packer test data for Olympic informatics Online Judge",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 1 - Planning",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
@@ -17,17 +28,8 @@ setup(
         "Programming Language :: Python :: Implementation :: CPython",
         # "Operating System :: OS Independent",
     ],
-    # description
-    description=
-    "a script can packer test data for Olympic informatics Online Judge",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     # about
     author="Simon_Chen",
     author_email="1020359403@qq.com",
     url="https://github.com/Simon-Chenzw/OJpacker",
-    license="LGPLv3",
-    entry_points={
-        'console_scripts': ['ojpacker = ojpacker:main'],
-    },
 )
